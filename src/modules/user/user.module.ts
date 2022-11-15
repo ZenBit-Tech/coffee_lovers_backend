5089055f6de2f7b17aa2a568e21f00009552a3eb
Pavlo Chernykh <95277626+pavlo-chernykh@users.noreply.github.com>
2022-11-15 20:19:01 +0200

    fixed absolutes order

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@/common/entities/User.entity';
+import { WorkHistory } from '@/common/entities/WorkHistory.entity';
+import { Education } from '@/common/entities/Education.entity';
 import { UserService } from '@/modules/user/user.service';
 import { UserController } from '@/modules/user/user.controller';
 import { MailModule } from '@/modules/mail/mail.module';
-import { WorkHistory } from '@/common/entities/WorkHistory.entity';
-import { Education } from '@/common/entities/Education.entity';
 
 @Module({
   imports: [
