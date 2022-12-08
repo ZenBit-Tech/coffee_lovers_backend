6ff90c867d3a728aa463ec8fcd484df515274947
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2022-12-08 17:39:27 +0200

    fix: added swagger

--- src/modules/chat/dto/create-message.dto.ts
+++ src/modules/chat/dto/create-message.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateMessageDto {
+  @ApiProperty({ example: 1 })
   @IsNumber()
   conversation: number;
 
+  @ApiProperty({ example: 'Hello, world!' })
   @IsString()
   message: string;
 }
--- src/modules/chat/dto/message.dto.ts
+++ src/modules/chat/dto/message.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
 import { CreateMessageDto } from './create-message.dto';
 
 export class MessageDto extends CreateMessageDto {
+  @ApiProperty({ example: 1 })
   @IsNumber()
   from: number;
 }
