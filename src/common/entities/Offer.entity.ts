0ea43183f8a25bd1be2d1fd9aa5edbf096d46bf3
Serhii Kukharchuk <kukharchuk.serg@gmail.com>
2022-12-26 16:06:49 +0200

    fix: offer entity relations

--- src/common/entities/Offer.entity.ts
+++ src/common/entities/Offer.entity.ts
@@ -2,15 +2,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  JoinColumn,
   ManyToOne,
-  OneToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Job } from '@entities/Job.entity';
 import { User } from '@entities/User.entity';
 import { OfferStatus } from '@constants/entities';
-import { Contract } from './Contract.entity';
 
 @Entity()
 export class Offer {
@@ -39,10 +36,6 @@ export class Offer {
 
   @CreateDateColumn()
   start: Date;
-
-  @OneToOne(() => Contract, (contract) => contract.offer)
-  @JoinColumn()
-  contract: Contract;
 
   @CreateDateColumn()
   created_at: Date;
